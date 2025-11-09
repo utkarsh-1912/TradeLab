1c2ec764426c6f56efb80ff18958a2e148e94b88
Utkarsh Gupta <79002186+utkarsh-1912@users.noreply.github.com>
2025-11-09 22:00:14 +0530

    add-changes

--- client/src/components/order-entry-modal.tsx
+++ client/src/components/order-entry-modal.tsx
@@ -112,6 +112,7 @@ export function OrderEntryModal({ onSubmit, disabled }: OrderEntryModalProps) {
         <Button 
           data-testid="button-new-order" 
           disabled={disabled}
+          size="sm"
           className="gap-2"
         >
           <Plus className="h-4 w-4" />
